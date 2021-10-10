5224b1ec2e484bfaf7905490a65e7e740c4bc12c
Akash R <me@akashirl.live>
2021-10-10 15:47:28 +0530

    completed left side of topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,26 @@
 import './topbar.scss';
-
+import { Person, Mail } from '@mui/icons-material';
 
 export default function Topbar() {
     return (
         <div className="topbar" id="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro"></a>
+                    <a href="#intro" className="logo">akash.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+91 9068041709</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>me@akashirl.live</span>
+                    </div>
                 </div>
+
                 <div className="right">
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
