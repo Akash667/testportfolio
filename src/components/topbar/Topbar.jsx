013074e222b6964489b96538d9ede6c9d6e7a153
Akash R <me@akashirl.live>
2021-10-10 13:50:07 +0530

    added global.scss, id to each component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,8 +3,15 @@ import './topbar.scss';
 
 export default function Topbar() {
     return (
-        <div className="topbar" >
-            
+        <div className="topbar" id="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro"></a>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     )
 }
